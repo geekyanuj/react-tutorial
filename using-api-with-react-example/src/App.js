8c5ae6e1629a91192e3f76208abf186deca15143
bytecodeanuj <anujv8692@gmail.com>
2024-12-23 15:20:00 +0530

    Added api-with-react-example

--- using-api-with-react-example/src/App.js
+++ using-api-with-react-example/src/App.js
@@ -3,7 +3,7 @@
 function App() {
   return (
     <div className="App">
-      
+      <h1> App</h1>
     </div>
   );
 }
---
+++ using-api-with-react-example/src/api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const searchImages = async ()=>{
+   const response = await axios.get('https://api.unsplash.com/search/photos',{
+        headers:{
+            Authorization: 'Client-ID XFbGsBLGXWzzdOo-wG93ag7feBzx3IVdJO5OIDM5OwI'
+        },
+        params:{
+            query: 'cars'
+        }
+    });
+
+    console.log(response);
+    
+    return response;
+};
+
+export default searchImages;
+
+
+
